8b11388c42582bf83587ef55a200c19a2e192bcf
ayanoaman3 <aya.amr.noaman@gmail.com>
2025-03-28 22:12:05 +0200

    Added calculator header file

---
+++ calculator.h
@@ -0,0 +1,9 @@
+#pragma once
+
+template<typename T> T add(const T&, const T&);
+template<typename T> T sub(const T&, const T&);
+template<typename T> T mult(const T&, const T&);
+template<typename T> T div(const T&, const T&);
+template<typename T> T fact(const T&, const T&);
+template<typename T> T gcd(const T&, const T&);
+template<typename T> T lcm(const T&, const T&);
