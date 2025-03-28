9ed5dc3637e4f80fd6a83b751e360883187b9e2c
ayanoaman3 <aya.amr.noaman@gmail.com>
2025-03-28 22:21:28 +0200

    defined 4 basic functions and fixed factorial declaration.

---
+++ src/calculator.cpp
@@ -0,0 +1,25 @@
+#include "calculator.h"
+
+
+template<typename T>
+T add(const T& a, const T& b) {
+    return a+b;
+}
+
+
+template<typename T>
+T sub(const T& a, const T& b) {
+    return a-b;
+}
+
+
+template<typename T>
+T mult(const T& a, const T& b) {
+    return a/b;
+}
+
+
+template<typename T>
+T div(const T& a, const T& b) {
+    return a/b;
+}
--- src/calculator.h
+++ src/calculator.h
@@ -4,7 +4,7 @@ template<typename T> T add(const T&, const T&);
 template<typename T> T sub(const T&, const T&);
 template<typename T> T mult(const T&, const T&);
 template<typename T> T div(const T&, const T&);
-template<typename T> T fact(const T&, const T&);
+template<typename T> T fact(const T&);
 template<typename T> T gcd(const T&, const T&);
 template<typename T> T lcm(const T&, const T&);
 template<typename T> T rand(const T&, const T&);
