6633d4ff53193c69b0bee5aec42a946d30f9b2c3
ayanoaman3 <aya.amr.noaman@gmail.com>
2025-03-30 02:25:06 +0200

    fixed mult implementation and overloaded fact to accept integer types only

--- src/calculator.cpp
+++ src/calculator.cpp
@@ -1,4 +1,5 @@
 #include <random>
+#include <type_traits>
 #include "calculator.h"
 
 
@@ -16,7 +17,7 @@ T sub(const T& a, const T& b) {
 
 template<typename T>
 T mult(const T& a, const T& b) {
-    return a/b;
+    return a*b;
 }
 
 
@@ -26,19 +27,97 @@ T div(const T& a, const T& b) {
 }
 
 
-template<typename T>
-T fact(const T& a) {
+////////////////////////////////////////////
+bigint fact(const int16_t& a) {
     if (a == 0) {
         return 1;
+    } else if (a < 0) {
+        return -1;
     }
 
-    T answer = 1;
+    bigint answer = 1;
     for (; a>0; --a) {
         answer *= a;
     }
 
     return answer;
 }
+
+bigint fact(const uint16_t& a) {
+    if (a == 0) {
+        return 1;
+    } else if (a < 0) {
+        return -1;
+    }
+
+    bigint answer = 1;
+    for (; a>0; --a) {
+        answer *= a;
+    }
+
+    return answer;
+}
+
+bigint fact(const int32_t& a) {
+    if (a == 0) {
+        return 1;
+    } else if (a < 0) {
+        return -1;
+    }
+
+    bigint answer = 1;
+    for (; a>0; --a) {
+        answer *= a;
+    }
+
+    return answer;
+}
+
+bigint fact(const uint32_t& a) {
+    if (a == 0) {
+        return 1;
+    } else if (a < 0) {
+        return -1;
+    }
+
+    bigint answer = 1;
+    for (; a>0; --a) {
+        answer *= a;
+    }
+
+    return answer;
+}
+
+bigint fact(const int64_t& a) {
+    if (a == 0) {
+        return 1;
+    } else if (a < 0) {
+        return -1;
+    }
+
+    bigint answer = 1;
+    for (; a>0; --a) {
+        answer *= a;
+    }
+
+    return answer;
+}
+
+bigint fact(const uint64_t& a) {
+    if (a == 0) {
+        return 1;
+    } else if (a < 0) {
+        return -1;
+    }
+
+    bigint answer = 1;
+    for (; a>0; --a) {
+        answer *= a;
+    }
+
+    return answer;
+}
+////////////////////////////////////////////
 
 
 template<typename T>
@@ -62,10 +141,20 @@ T lcm(const T& a, const T& b) {
 
 // template<typename T>
 // T rand(const T& a, const T& b) {
+//     bigint a2 = bigint(a);
+//     bigint b2 = bigint(b);
+
+//     int lenA = a2.length();
+//     int lenB = b3.length();
+
+//     if (lenA > lenB) {
+//         for (int i = 0; i < lenA-lenB; ++i) {
+
+//         }
+//     }
 //     std::random_device rd;
 //     std::mt19937 gen(rd());
-//     if ( type(a))  // work on checking type
 //     std::uniform_real_distribution<T> distr(a, b);
 
 //     return T(distr(gen));
-// }
+}
